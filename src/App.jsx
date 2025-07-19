051bfabdecf618fa8e744399782e532a5d99eccc
franci-maci <118402146+Franci-maci@users.noreply.github.com>
2025-07-19 04:58:06 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className="container mt-4">
+        <h1>Welcome to My React App!</h1>
+        <p>This is a Bootstrap navbar using React and Vite.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
